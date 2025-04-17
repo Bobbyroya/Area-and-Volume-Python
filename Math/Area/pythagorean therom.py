fe8099370d0d73164ca9d63642398521a6622d34
Bobby <161744355+Bobbyroya@users.noreply.github.com>
2025-04-17 13:26:13 -0600

    pythagorean therom.py

--- Math/Area/pythagorean therom.py
+++ Math/Area/pythagorean therom.py
@@ -1,5 +1,6 @@
+import math
 print("Calculate the Pythagorean Therom.")
 asquared = float(input("Enter the first side's length: "))
 bsquared = float(input("Enter the  second side's length: "))
-csquared = (asquared*asquared+bsquared*bsquared)
+c = math.sqrt((asquared*asquared+bsquared*bsquared))
 print(f"The answer is {csquared}")
